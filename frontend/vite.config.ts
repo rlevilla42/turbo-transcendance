d5e6df0b1747db4d75817fb6763dc86403a64e2c
rlevilla42 <rlevilla42@gmail.com>
2025-12-07 18:36:28 +0100

    hot reload fonctionelle

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
     server: {
         host: true, // Permet d'écouter sur 0.0.0.0
+        allowedHosts: ['46.224.45.217.sslip.io'],
         watch: {
             usePolling: true, // Force la vérification des fichiers (utile avec Docker/SFTP)
         },
