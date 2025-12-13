a0d149dbf5fe21d57e83c8ca87431820cbb7d781
rlevilla42 <rlevilla42@gmail.com>
2025-12-13 21:58:16 +0100

    erreur css bundle

--- frontend/src/pages/AuthPages/SignIn.tsx
+++ frontend/src/pages/AuthPages/SignIn.tsx
@@ -3,15 +3,15 @@ import AuthLayout from "./AuthPageLayout";
 import SignInForm from "../../components/auth/SignInForm";
 
 export default function SignIn() {
-  return (
-    <>
-      <PageMeta
-        title="React.js SignIn Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js SignIn Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
-      />
-      <AuthLayout>
-        <SignInForm />
-      </AuthLayout>
-    </>
-  );
-}git
+    return (
+        <>
+            <PageMeta
+                title="React.js SignIn Dashboard | TailAdmin - Next.js Admin Dashboard Template"
+                description="This is React.js SignIn Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+            />
+            <AuthLayout>
+                <SignInForm/>
+            </AuthLayout>
+        </>
+    );
+}
